d240b1106e78ddfb86f1816d14bf464019d4dc95
davidskyberg@gmail.com <davidskyberg@gmail.com>
2020-03-18 13:15:32 -0400

    Fixed what beautify broke.

--- src/style/withRoot.js
+++ src/style/withRoot.js
@@ -25,13 +25,11 @@ function withRoot(Component) {
     function WithRoot(props) {
         // MuiThemeProvider makes the theme available down the React tree
         // thanks to React context.
-        return ( <
-            MuiThemeProvider theme = { theme } > { /* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */ } <
-            CssBaseline / >
-            <
-            Component {...props }
-            /> < /
-            MuiThemeProvider >
+        return (
+            <MuiThemeProvider theme = { theme } >
+                <CssBaseline />
+                <Component {...props }/>
+            < /MuiThemeProvider >
         );
     }
 
